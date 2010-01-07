3bf033f5b0c714990e45fb7ca1d4ea5fa96c3937
Dzonny_cp <SND\Dzonny_cp>
2010-01-07 13:34:57 -0800

    Localiyation tables

--- CapsData/Change Scripts/0.1.3.0-to-0.1.4.0.sql
+++ CapsData/Change Scripts/0.1.3.0-to-0.1.4.0.sql
@@ -270,6 +270,277 @@ BEGIN
 			 
 END
 GO
+----------------------------------------------------------------------------------------------------------------------------------
+-- CapTranslation table
+
+CREATE TABLE [dbo].[CapTranslation](
+	[CapTranslationID] [int] IDENTITY(1,1) NOT NULL,
+	[CapID] [int] NOT NULL,
+	[Culture] [varchar](15) NOT NULL,
+	[CapName] [nvarchar](255) NULL,
+	[MainPicture] [nvarchar](255) NULL,
+	[Note] [nvarchar](max) NULL,
+	[AnotherPictures] [nvarchar](max) NULL,
+ CONSTRAINT [PK_CapTranslation] PRIMARY KEY CLUSTERED 
+(
+	[CapTranslationID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
+ CONSTRAINT [UNQ_trnCap] UNIQUE NONCLUSTERED 
+(
+	[CapID] ASC,
+	[Culture] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[CapTranslation]  WITH CHECK ADD  CONSTRAINT [FK_CapTranslation_Cap] FOREIGN KEY([CapID])
+REFERENCES [dbo].[Cap] ([CapID])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[CapTranslation] CHECK CONSTRAINT [FK_CapTranslation_Cap]
+GO
+
+ALTER TABLE [dbo].[CapTranslation]  WITH CHECK ADD  CONSTRAINT [CHK_CapTranslation_NoEmptyStrings] CHECK  (([Culture]<>'' AND [CapName]<>'' AND [MainPicture]<>'' AND [Note]<>'' AND [AnotherPictures]<>''))
+GO
+
+ALTER TABLE [dbo].[CapTranslation] CHECK CONSTRAINT [CHK_CapTranslation_NoEmptyStrings]
+GO
+
+--Triggers
+CREATE TRIGGER [dbo].[CapTranslation_Instead_Ins] 
+   ON  [dbo].CapTranslation 
+   INSTEAD OF INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+    INSERT INTO dbo.CapTranslation
+	    (CapID, Culture, CapName, MainPicture, Note, AnotherPictures)
+    OUTPUT inserted.*
+	SELECT CapID, dbo.EmptyStrToNull(Culture),dbo.EmptyStrToNull(CapName),dbo.EmptyStrToNull(MainPicture),dbo.EmptyStrToNull(Note),dbo.EmptyStrToNull(AnotherPictures)
+    FROM inserted;
+END
+GO
+CREATE TRIGGER [dbo].[CapTranslation_Instead_Upd]
+   ON [dbo].[CapTranslation] 
+   INSTEAD OF UPDATE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE [dbo].CapTranslation
+	SET	
+	CapID=i.CapID,
+	Culture=dbo.EmptyStrToNull(i.Culture),
+	CapName=dbo.EmptyStrToNull(i.CapName),
+	MainPicture=dbo.EmptyStrToNull(i.MainPicture),
+	Note=dbo.EmptyStrToNull(i.Note),
+	AnotherPictures=dbo.EmptyStrToNull(i.AnotherPictures)
+	FROM inserted AS i
+	WHERE CapTranslation.CapTranslationID=i.CapTranslationID;			 
+END;
+GO
+
+-------------------------------------------------------------------------------------------------------------------------------
+-- SimpleTranslation table
+
+CREATE TABLE [dbo].[SimpleTranslation](
+	[SimpleTranslationID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryID] [int] NULL,
+	[KeywordID] [int] NULL,
+	[ProductID] [int] NULL,
+	[CompanyID] [int] NULL,
+	[ProductTypeID] [int] NULL,
+	[TargetID] [int] NULL,
+	[MaterialID] [int] NULL,
+	[CapTypeID] [int] NULL,
+	[ShapeID] [int] NULL,
+	[MainTypeID] [int] NULL,
+	[CapSignID] [int] NULL,
+	[StorageID] [int] NULL,
+	[StorageTypeID] [int] NULL,
+	[CapInstanceID] [int] NULL,
+	[Culture] [varchar](15) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_SimpleTranslation] PRIMARY KEY CLUSTERED 
+(
+	[SimpleTranslationID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
+ CONSTRAINT [UNQ_SimpleTranslation] UNIQUE NONCLUSTERED 
+(
+	[CategoryID] ASC,
+	[KeywordID] ASC,
+	[ProductID] ASC,
+	[ProductTypeID] ASC,
+	[TargetID] ASC,
+	[MaterialID] ASC,
+	[CapTypeID] ASC,
+	[ShapeID] ASC,
+	[MainTypeID] ASC,
+	[CapSignID] ASC,
+	[StorageID] ASC,
+	[StorageTypeID] ASC,
+	[CompanyID] ASC,
+	[Culture] ASC,
+	[CapInstanceID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_CapInstance] FOREIGN KEY([CapInstanceID])
+REFERENCES [dbo].[CapInstance] ([CapInstanceID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_CapInstance]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_CapSign] FOREIGN KEY([CapSignID])
+REFERENCES [dbo].[CapSign] ([CapSignID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_CapSign]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_CapType] FOREIGN KEY([CapTypeID])
+REFERENCES [dbo].[CapType] ([CapTypeID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_CapType]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Category] FOREIGN KEY([CategoryID])
+REFERENCES [dbo].[Category] ([CategoryID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Category]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Company] FOREIGN KEY([CompanyID])
+REFERENCES [dbo].[Company] ([CompanyID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Company]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Keyword] FOREIGN KEY([KeywordID])
+REFERENCES [dbo].[Keyword] ([KeywordID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Keyword]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_MainType] FOREIGN KEY([MainTypeID])
+REFERENCES [dbo].[MainType] ([MainTypeID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_MainType]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Material] FOREIGN KEY([MaterialID])
+REFERENCES [dbo].[Material] ([MaterialID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Material]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Product] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ProductID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Product]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_ProductType] FOREIGN KEY([ProductTypeID])
+REFERENCES [dbo].[ProductType] ([ProductTypeID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_ProductType]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Shape] FOREIGN KEY([ShapeID])
+REFERENCES [dbo].[Shape] ([ShapeID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Shape]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Storage] FOREIGN KEY([StorageID])
+REFERENCES [dbo].[Storage] ([StorageID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Storage]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_StorageType] FOREIGN KEY([StorageTypeID])
+REFERENCES [dbo].[StorageType] ([StorageTypeID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_StorageType]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [FK_SimpleTranslation_Target] FOREIGN KEY([TargetID])
+REFERENCES [dbo].[Target] ([TargetID])
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [FK_SimpleTranslation_Target]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [CHK_SimpleTranslation_NoEmptyStrings] CHECK  (([Culture]<>'' AND [Name]<>'' AND [Description]<>''))
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [CHK_SimpleTranslation_NoEmptyStrings]
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation]  WITH CHECK ADD  CONSTRAINT [CHK_SimpleTranslation_OnlyOneParent] CHECK  (((((((((((((((case when [CategoryID] IS NULL then (0) else (1) end+case when [KeywordID] IS NULL then (0) else (1) end)+case when [ProductID] IS NULL then (0) else (1) end)+case when [CompanyID] IS NULL then (0) else (1) end)+case when [ProductTypeID] IS NULL then (0) else (1) end)+case when [TargetID] IS NULL then (0) else (1) end)+case when [MaterialID] IS NULL then (0) else (1) end)+case when [CapTypeID] IS NULL then (0) else (1) end)+case when [ShapeID] IS NULL then (0) else (1) end)+case when [MainTypeID] IS NULL then (0) else (1) end)+case when [CapSignID] IS NULL then (0) else (1) end)+case when [StorageID] IS NULL then (0) else (1) end)+case when [StorageTypeID] IS NULL then (0) else (1) end)+case when [CapInstanceID] IS NULL then (0) else (1) end)=(1)))
+GO
+
+ALTER TABLE [dbo].[SimpleTranslation] CHECK CONSTRAINT [CHK_SimpleTranslation_OnlyOneParent]
+GO 
+--Triggers
+CREATE TRIGGER [dbo].[SimpleTranslation_Instead_Ins] 
+   ON  [dbo].SimpleTranslation 
+   INSTEAD OF INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO dbo.SimpleTranslation
+		(CategoryID, KeywordID, ProductID, CompanyID, ProductTypeID, TargetID, MaterialID, CapTypeID, ShapeID, MainTypeID, CapSignID, StorageID, StorageTypeID, CapInstanceID, Culture, Name, [Description])
+        OUTPUT inserted.*
+		SELECT CategoryID, KeywordID, ProductID, CompanyID, ProductTypeID, TargetID, MaterialID, CapTypeID, ShapeID, MainTypeID, CapSignID, StorageID, StorageTypeID, CapInstanceID, dbo.EmptyStrToNull(Culture), dbo.EmptyStrToNull(Name), dbo.EmptyStrToNull([Description])
+	FROM inserted;
+END
+GO
+CREATE TRIGGER [dbo].[SimpleTranslation_Instead_Upd]
+   ON [dbo].[SimpleTranslation] 
+   INSTEAD OF UPDATE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE [dbo].SimpleTranslation
+	SET	
+		CategoryID    = i.CategoryID,
+		KeywordID     = i.KeywordID,
+		ProductID     = i.ProductID,
+		CompanyID     = i.CompanyID,
+		ProductTypeID = i.ProductTypeID,
+		TargetID      = i.TargetID,
+		MaterialID    = i.MaterialID,
+		CapTypeID     = i.CapTypeID,
+		ShapeID       = i.ShapeID,
+		MainTypeID    = i.MainTypeID,
+		CapSignID     = i.CapSignID,
+		StorageID     = i.StorageID,
+		StorageTypeID = i.StorageTypeID,
+		CapInstanceID = i.CapInstanceID,
+		Culture       = dbo.EmptyStrToNull(i.Culture),
+		Name          = dbo.EmptyStrToNull(i.Name),
+		[Description] = dbo.EmptyStrToNull(i.[Description])
+	FROM inserted AS i
+	WHERE SimpleTranslation.SimpleTranslationID=i.SimpleTranslationID;			 
+END;
+GO
 
 --------------------------------------------------------------------------------------------------------------------------------
 --Increase version
