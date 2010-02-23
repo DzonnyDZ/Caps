f2f824f5de6c0ac29c9998ef48ec5392f9ca66f4
Dzonny_cp <SND\Dzonny_cp>
2010-02-22 16:35:09 -0800

    Customize t4 template for CapsData.edmx

---
+++ Caps.Common/DAL/CapsData.Designer.vb
@@ -0,0 +1,4 @@
+﻿' Default code generation is disabled for model 'D:\Users\Honza\Documents\Programy\Caps\Caps.Common\DAL\CapsData.edmx'. 
+' To enable default code generation, change the value of the 'Code Generation Strategy' designer
+' property to an alternate value. This property is available in the Properties Window when the model is
+' open in the designer.
